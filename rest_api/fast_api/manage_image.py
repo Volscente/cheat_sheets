4d7c1d93710cf057869fbd587f508930225b5eee
Simone Porreca <porrecasimone@gmail.com>
2022-10-26 19:56:00 +0200

    FastAPI - Manage image OpenCV

--- rest_api/fast_api/manage_image.py
+++ rest_api/fast_api/manage_image.py
@@ -3,16 +3,17 @@ from fastapi import FastAPI, UploadFile, File
 import cv2
 import numpy as np
 
+# Instance FastAPI object
 app = FastAPI()
 
 
 @app.post('/file')
 def upload_image(image_file: UploadFile = File(...)):
 
-    # image = cv2.imread(image_file.file.read())
+    # Read the 'SpooledTemporaryFile' in image_file.file.read() as Numpy Array
+    numpy_array = np.frombuffer(image_file.file.read(), np.uint8)
 
-    nparr = np.frombuffer(image_file.file.read(), np.uint8)
-
+    # Read OpenCV Image
     image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
 
     return {'image_file_type': image.shape}
