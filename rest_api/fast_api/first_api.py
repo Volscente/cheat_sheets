3358e303afdea2b4d6c234cf7bc1a59be292390d
Simone Porreca <porrecasimone@gmail.com>
2022-11-22 22:07:53 +0100

    FastAPI - Docstring

--- rest_api/fast_api/first_api.py
+++ rest_api/fast_api/first_api.py
@@ -5,9 +5,11 @@ from fastapi import FastAPI
 service = FastAPI()
 
 
+# The first term of the decorator 'service' has to be equal to the FastAPI object instantiated
 @service.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @service.get('/items/{item_id}')
 async def get_item(item_id: str):
