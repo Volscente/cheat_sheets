af5884a397985fe584084006dbb56f7f6ce0f012
Simone Porreca <porrecasimone@gmail.com>
2022-11-26 20:20:47 +0100

    FastAPI - Enum Path Parameters

--- rest_api/fast_api/2_path_parameters.py
+++ rest_api/fast_api/2_path_parameters.py
@@ -5,7 +5,8 @@ from enum import Enum
 
 class Animal(str, Enum):
     """
-    Define a class holding possible values for the 'animal' path parameter. No other values would be accepted
+    Define a class holding possible values for the 'animal' path parameter. No other values would be accepted.
+    The class Animal inherits both from str and Enum.
     """
 
     cat = 'Cat'
@@ -19,12 +20,19 @@ app = FastAPI()
 
 @app.get('/animals/{animal}')
 async def get_animal(animal: Animal):
+    """
+    It accepts a path parameter 'animal' that comes from an Animal str enum class of possible values (Cat, Dog and Bird)
 
-    match animal:
-        case Animal.cat:
-            return {'animal': animal, 'message': 'You are looking for a cat'}
-        case Animal.dog:
-            return {'animal': animal, 'message': 'You are looking for a dog'}
-        case Animal.bird:
-            return {'animal': animal, 'message': 'You are looking for a bird'}
+    Args:
+        animal: Animal string enum of animals
 
+    Returns:
+        Dict animal string and message string
+    """
+    return {'animal': animal, 'message': 'You are looking for a {}'.format(animal)}
+
+
+@app.get('/files/{file_path:path}')
+async def get_files(file_path: str):
+
+    return {'file_path': file_path}
