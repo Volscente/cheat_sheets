cd7bf495c00c3c482db630eb0938ba0ceed927af
Simone Porreca <porrecasimone@gmail.com>
2022-11-27 15:11:04 +0100

    FastAPI - Query param with Enum

--- rest_api/fast_api/3_query_parameters.py
+++ rest_api/fast_api/3_query_parameters.py
@@ -1,8 +1,18 @@
 # Import Standard Libraries
 from fastapi import FastAPI
+from enum import Enum
 
 # Instantiate FastAPI
 app = FastAPI()
+
+
+class PackageSize(str, Enum):
+    """
+    Class that implements possible Package size
+    """
+    small = 'Small'
+    medium = 'Medium'
+    large = 'Large'
 
 
 @app.get('/items/{item_id}')
@@ -53,3 +63,20 @@ async def read_user_item(user_id: int,
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+@app.get('/packages/{package_id}')
+async def get_package(package_id: int,
+                      size: PackageSize):
+    """
+    Retrieve package
+
+    Args:
+        package_id: Integer package id
+        size: PackageSize enum required query parameter
+
+    Returns:
+    """
+
+    return {'package_id': package_id,
+            'size': size}
