74cb49f18ef7a6f149c76d92a7c01a6f0f982a35
Simone Porreca <porrecasimone@gmail.com>
2022-12-11 16:54:55 +0100

    FastAPI - Query Parameter Validation

--- rest_api/fast_api/5_query_parameters_validation.py
+++ rest_api/fast_api/5_query_parameters_validation.py
@@ -2,17 +2,42 @@
 from fastapi import FastAPI, Query
 
 # Instantiate FastAPI
-# app = FastAPI()
+app = FastAPI()
 
 
-# Define GET method with a validation on Query Parameter 'description' to not exceed 50 characters
+# Define GET method with a validation on Optional Query Parameter 'description' between 3 and 50 characters and starting with 'C'
 @app.get('/items/{item_id}')
 async def get_items(item_id: int,
-                    description: str | None = Query(default=None, max_length=50)):
+                    description: str | None = Query(default=None, # You can pass whatever default value, also a String
+                                                    min_length=3,
+                                                    max_length=50,
+                                                    regex="^C")):
 
     # Define the Response Body
     response_body = {'item_id': item_id}
 
     if description:
 
-        response_body.update()
+        response_body.update({'description': description})
+
+    return response_body
+
+
+# Define the Required Query Parameter that should start with a capital letter and be at least 2 characters
+@app.get('/users/')
+async def get_users(name: str = Query(min_length=2, # You can also specify the mandatory field as 'default=...'
+                                      regex="^[A-Z]")):
+
+    # Define the response body
+    response_body = {'name': name}
+
+    return response_body
+
+
+# Define a Query Parameter of type List
+@app.get('/user_list/')
+async def get_user_list(users: list[str] | None = Query(default=['john', 'sam'])):
+
+    return {'users': users}
+
+# TODO Path Parameters and Numeric Validations
