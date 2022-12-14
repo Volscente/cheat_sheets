6a1f6c33b4978aef573fb9741416a07a4eca9813
Simone Porreca <porrecasimone@gmail.com>
2022-12-14 12:46:43 +0100

    FastAPI - Multiple Body Request Parameters

--- rest_api/fast_api/4_request_body.py
+++ rest_api/fast_api/4_request_body.py
@@ -1,16 +1,26 @@
 # Import Standard Libraries
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
     """
-    Request body defined through Pydantic
+    Request body parameter defined through Pydantic
     """
 
     id: int
     price: float
     description: str | None = None
+
+
+class User(BaseModel):
+    """
+    Request body parameter defined through Pydantic
+    """
+
+    user_id: int
+    name: str
+    age: int | None = None
 
 
 # Instantiate FastAPI
@@ -36,5 +46,23 @@ async def create_item(item_id: int,
 
     if note:
         response_body.update({'note': note})
+
+    return response_body
+
+
+# Multiple Request Body parameters
+# NOTE: In case you want to add another Request Body parameter without defining its Pydantic model, use the Body() object
+@app.post('/item_and_user/')
+async def create_item_and_user(item: Item,
+                               user: User,
+                               importance: int | None = Body()):
+
+    # Define the Response Body
+    response_body = {'item_id': item.id,
+                     'user_id': user.user_id}
+
+    if importance:
+
+        response_body.update({'importance': importance})
 
     return response_body
