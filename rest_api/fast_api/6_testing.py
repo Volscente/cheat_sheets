e110e195a5b47e031b8b29e257f3351a77d1799a
Simone Porreca <porrecasimone@gmail.com>
2022-12-24 14:19:50 +0100

    FastAPI - Define upload_image for testing

--- rest_api/fast_api/6_testing.py
+++ rest_api/fast_api/6_testing.py
@@ -14,17 +14,32 @@ test_client = TestClient(app)
 
 
 @app.post('/upload_image/')
-async def upload_image(image: UploadFile = File(...,
+async def upload_image(image_file: UploadFile = File(...,
                                                 description='Image file from which detect the object')):
     """
-    Upload an image and returns the shape
+    Upload an image and returns the Blob shape
 
     Args:
-        image: UploadFile request body image file
+        image_file: UploadFile request body image file
 
     Returns:
-
+        blob_shape: Tuple[int] Blob shape
     """
+
+    # Read the 'SpooledTemporaryFile' in image_file.file.read() as Numpy Array
+    numpy_array = np.frombuffer(image_file.file.read(), np.uint8)
+
+    # Read OpenCV Image
+    image = cv2.imdecode(numpy_array, cv2.IMREAD_UNCHANGED)
+
+    # Read blob from image
+    blob = cv2.dnn.blobFromImage(image=image,
+                                 size=(416, 416),
+                                 scalefactor=1 / 255.,
+                                 swapRB=True,
+                                 crop=False)
+
+    return {'blob_shape': blob.shape}
 
 
 @pytest.mark.parametrize('test_file, expected_output', [
