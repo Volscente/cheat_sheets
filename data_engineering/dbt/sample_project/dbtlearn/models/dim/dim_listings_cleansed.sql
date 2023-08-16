5f87d1d81a9f7571041700488eababb36bfeef74
Simone Porreca <porrecasimone@gmail.com>
2023-08-16 21:37:57 +0200

    DE - DBT add dim_hosts and dim_listings

--- data_engineering/dbt/sample_project/dbtlearn/models/dim/dim_listings_cleansed.sql
+++ data_engineering/dbt/sample_project/dbtlearn/models/dim/dim_listings_cleansed.sql
@@ -4,3 +4,23 @@ WITH src_listings AS (
     FROM
         {{ ref('src_listings') }} -- This is a reference to the model src_listings (Jinja syntax)
 )
+SELECT
+    listing_id,
+    listing_name,
+    room_type,
+    CASE
+        WHEN minimum_nights = 0 THEN 1
+        ELSE minimum_nights
+    END AS minimum_nights,
+    host_id,
+    REPLACE(
+        price_str,
+        '$' -- Replace the dollar sign with nothing
+    ) :: NUMBER( -- Convert through "::" operator the string to a number of 2 decimal places
+        10,
+        2
+    ) AS price,
+    created_at,
+    updated_at
+FROM
+ src_listings
