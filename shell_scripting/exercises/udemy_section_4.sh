914dc15cd5950e19237befbfbfed1a35a99813f2
Simone Porreca <porrecasimone@gmail.com>
2024-01-09 17:12:19 +0100

    Update udemy_section_4.sh

--- shell_scripting/exercises/udemy_section_4.sh
+++ shell_scripting/exercises/udemy_section_4.sh
@@ -4,15 +4,24 @@
 
 function file_count(){
 
+    local DIR_PATH=${1}
+
     # Check if the argument is pased
-    if [ -z ${1} ]
+    if [ -z ${DIR_PATH} ]
     then
         echo "No directory path is passed."
         return 1
     fi
     
     # TODO Check if it is a directory
-
+    if [ -d ${DIR_PATH} ]
+    then
+        echo "${DIR_PATH}:"
+        ls ${DIR_PATH} | wc -l
+    else
+        echo "The passed path is not a directory,"
+        exit 2
+    fi
 }
 
 file_count ${1}
