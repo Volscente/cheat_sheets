5a3df5ebfe560b365a67a1646171e9f562e96fe3
Simone Porreca <porrecasimone@gmail.com>
2024-02-23 23:11:36 +0100

    Update example_pipeline.py

--- machine_learning/Kubeflow/code/example_pipeline.py
+++ machine_learning/Kubeflow/code/example_pipeline.py
@@ -1,5 +1,6 @@
 # Import Standard Libraries
 from kpf import dsl, compiler
+from kpf.client import Client
 
 @dsl.component
 def example_component(name: str) -> str:
@@ -20,3 +21,11 @@ def example_pipeline(name: str) -> str:
 
 # Compile the pipeline into a .YAML file
 compiler.Compiler().compile(example_pipeline, './../compiled_pipelines/example_pipeline.yaml')
+
+# Instance a Kubeflow Client where to submit the pipeline (e.g., GCP Vertex AI)
+client = Client(host='<endpoint>')
+
+# Create the pipeline
+run = client.create_run_from_pipeline_package(
+    './../compiled_pipelines/example_pipeline.yaml', 
+    arguments={'recipient': 'World'})
