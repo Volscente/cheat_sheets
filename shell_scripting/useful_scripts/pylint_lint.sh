90b1d9318b516796d2e0afc47162e1da0d07f645
Simone Porreca <porrecasimone@gmail.com>
2025-05-26 19:16:17 +0200

    Update pylint_lint.sh

--- shell_scripting/useful_scripts/pylint_lint.sh
+++ shell_scripting/useful_scripts/pylint_lint.sh
@@ -2,15 +2,19 @@
 #
 # Run PyLint with specific plugins loaded and message template
 
+echo
 echo "-------- PyLint Lint --------"
 echo
 
 # Lint
 poetry run pylint --load-plugins pylint_pytest \
+  --ignore-patterns=".*components.*|.*pipelines.*" \
+  --disable='C0301, R0903, R0801, W0212, W0511' \
   --source-roots=./src \
   --output-format=colorized \
   --msg-template='Rule: {msg_id} - Position: [{line},{column}] -  {msg}' \
   ./src ./tests
 
+echo
 echo "--------------------------------"
 echo
